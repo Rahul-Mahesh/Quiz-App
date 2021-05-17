f1bc001f80a57b36ca3455159ccb8434604c5fd2
Rahul-Mahesh <rahul240at@gmail.com>
2021-05-17 22:28:05 +0530

    added

---
+++ js/questions.js
@@ -0,0 +1,48 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "A bird which can fly back wards ?",
+    answer: "Humming Bird",
+    options: [
+      "Wood Peaker ",
+      "Sparrow",
+      "Humming Bird",
+      "Parrot"
+    ]
+  },
+    {
+    numb: 2,
+    question: "which is the tallest skyscraper in the world ?",
+    answer: "Burj Khalifa",
+    options: [
+      "King tower",
+      "Burj Khalifa",
+      "White House",
+      "Shanghai Tower"
+    ]
+  },
+    {
+    numb: 3,
+    question: "what is the fastest train in world ?",
+    answer: "Maglev bullet train",
+    options: [
+      "Maglev bullet train",
+      "Fuxing Hao CR400AF",
+      "Shinkansen H5 and E5",
+      "Thalys"
+    ]
+  },
+
+  {
+  numb: 4,
+  question: "What’s the smallest country in the world?",
+  answer: "The Vatican",
+  options: [
+    "Monaco",
+     "The Vatican",
+    "San Marino",
+    "Maldives"
+  ]
+ },
+];
